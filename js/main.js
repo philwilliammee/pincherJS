cdc7691a39650ff53423c906be8078b9538f4660
philwilliammee <philliammee@yahoo.com>
2016-11-10 06:34:40 +0300

    combined templates in master

--- js/main.js
+++ js/main.js
@@ -13,58 +13,109 @@
         var container = document.getElementById('myCanvas');
         container.appendChild(pincherDom);
         //render the doms
-        $("#navTemplate").load("./templates/nav.html", function (tmpl) {
-            NavView.prototype.template = Handlebars.compile($("#navbar-tpl").html());
-            $("#navTemplate").html(new NavView(service).render().$el);
-        });
+        /**
+         $("#navTemplate").load("./templates/nav.html", function (tmpl) {
+         NavView.prototype.template = Handlebars.compile($("#navbar-tpl").html());
+         $("#navTemplate").html(new NavView(service).render().$el);
+         });
+         
+         $("#sideBarTemplate").load("./templates/sideBar.html", function (tmpl) {
+         SideBarView.prototype.template = Handlebars.compile($("#sideBar-tpl").html());
+         EditPoseMdalContentView.prototype.template = Handlebars.compile($("#editPoseModal-tpl").html());
+         $("#sideBarTemplate").html(new SideBarView(service).render().$el);
+         
+         // test the pose editor
+         if (service.pincher) {
+         functionalTest(service);
+         //service.buildPosetest();
+         service.buildMotortest();
+         } else {
+         console.log("Pincher arm not initialized");
+         }
+         
+         });
+         
+         $("#runSequenceTemplate").load("./templates/runSequencePanel.html", function (tmpl) {
+         RunSequenceView.prototype.template = Handlebars.compile($("#runSequence-tpl").html());
+         $("#runSequenceTemplate").html(new RunSequenceView(service).render().$el);
+         });
+         
+         $("#panel1Template").load("./templates/panel1.html", function (tmpl) {
+         GUI_View.prototype.template = Handlebars.compile($("#panel1-tpl").html());
+         $("#panel1Template").html(new GUI_View(service).render().$el);
+         //@ todo render GUI inside panel1View 
+         //build and attach the IK controller GUI
+         service.gui = new GUI(service);
+         service.gui.gui.domElement.id = "gui";
+         $("#gui").addClass("gui");
+         $("#gui").detach().appendTo("#kinematicsGUI");
+         //build and attach the Motors controller GUI 
+         service.ax_gui = new ax_GUI(service);
+         service.ax_gui.gui.domElement.id = "ax_gui";
+         $("#ax_gui").addClass("gui");
+         $("#ax_gui").detach().appendTo("#motorsGUI");
+         //build and attach the Radian controller GUI
+         service.rads_gui = new radsGUI(service);
+         service.rads_gui.gui.domElement.id = "rads_gui";
+         $("#rads_gui").addClass("gui");
+         $("#rads_gui").detach().appendTo("#radsGUI");
+         
+         });
+         
+         $("#panel2Template").load("./templates/panel2.html", function (tmpl) {
+         Panel2View.prototype.template = Handlebars.compile($("#panel2-tpl").html());
+         $("#panel2Template").html(new Panel2View(service).render().$el);
+         });
+         **/
 
-        $("#sideBarTemplate").load("./templates/sideBar.html", function (tmpl) {
-            SideBarView.prototype.template = Handlebars.compile($("#sideBar-tpl").html());
-            EditPoseMdalContentView.prototype.template = Handlebars.compile($("#editPoseModal-tpl").html());
-            $("#sideBarTemplate").html(new SideBarView(service).render().$el);
 
-            // test the pose editor
-            if (service.pincher) {
-                functionalTest(service);
-                //service.buildPosetest();
-                service.buildMotortest();
-            } else {
-                console.log("Pincher arm not initialized");
-            }
+        NavView.prototype.template = Handlebars.compile($("#navbar-tpl").html());
+        $("#navTemplate").html(new NavView(service).render().$el);
 
-        });
 
-        $("#runSequenceTemplate").load("./templates/runSequencePanel.html", function (tmpl) {
-            RunSequenceView.prototype.template = Handlebars.compile($("#runSequence-tpl").html());
-            $("#runSequenceTemplate").html(new RunSequenceView(service).render().$el);
-        });
 
-        $("#panel1Template").load("./templates/panel1.html", function (tmpl) {
-            GUI_View.prototype.template = Handlebars.compile($("#panel1-tpl").html());
-            $("#panel1Template").html(new GUI_View(service).render().$el);
-            //@ todo render GUI inside panel1View 
-            //build and attach the IK controller GUI
-            service.gui = new GUI(service);
-            service.gui.gui.domElement.id = "gui";
-            $("#gui").addClass("gui");
-            $("#gui").detach().appendTo("#kinematicsGUI");
-            //build and attach the Motors controller GUI 
-            service.ax_gui = new ax_GUI(service);
-            service.ax_gui.gui.domElement.id = "ax_gui";
-            $("#ax_gui").addClass("gui");
-            $("#ax_gui").detach().appendTo("#motorsGUI");
-            //build and attach the Radian controller GUI
-            service.rads_gui = new radsGUI(service);
-            service.rads_gui.gui.domElement.id = "rads_gui";
-            $("#rads_gui").addClass("gui");
-            $("#rads_gui").detach().appendTo("#radsGUI");
+        SideBarView.prototype.template = Handlebars.compile($("#sideBar-tpl").html());
+        EditPoseMdalContentView.prototype.template = Handlebars.compile($("#editPoseModal-tpl").html());
+        $("#sideBarTemplate").html(new SideBarView(service).render().$el);
 
-        });
+        // test the pose editor
+        if (service.pincher) {
+            functionalTest(service);
+            //service.buildPosetest();
+            service.buildMotortest();
+        } else {
+            console.log("Pincher arm not initialized");
+        }
 
-        $("#panel2Template").load("./templates/panel2.html", function (tmpl) {
-            Panel2View.prototype.template = Handlebars.compile($("#panel2-tpl").html());
-            $("#panel2Template").html(new Panel2View(service).render().$el);
-        });
+
+
+        RunSequenceView.prototype.template = Handlebars.compile($("#runSequence-tpl").html());
+        $("#runSequenceTemplate").html(new RunSequenceView(service).render().$el);
+
+
+        GUI_View.prototype.template = Handlebars.compile($("#panel1-tpl").html());
+        $("#panel1Template").html(new GUI_View(service).render().$el);
+        //@ todo render GUI inside panel1View 
+        //build and attach the IK controller GUI
+        service.gui = new GUI(service);
+        service.gui.gui.domElement.id = "gui";
+        $("#gui").addClass("gui");
+        $("#gui").detach().appendTo("#kinematicsGUI");
+        //build and attach the Motors controller GUI 
+        service.ax_gui = new ax_GUI(service);
+        service.ax_gui.gui.domElement.id = "ax_gui";
+        $("#ax_gui").addClass("gui");
+        $("#ax_gui").detach().appendTo("#motorsGUI");
+        //build and attach the Radian controller GUI
+        service.rads_gui = new radsGUI(service);
+        service.rads_gui.gui.domElement.id = "rads_gui";
+        $("#rads_gui").addClass("gui");
+        $("#rads_gui").detach().appendTo("#radsGUI");
+
+
+
+        Panel2View.prototype.template = Handlebars.compile($("#panel2-tpl").html());
+        $("#panel2Template").html(new Panel2View(service).render().$el);
 
     });
 
